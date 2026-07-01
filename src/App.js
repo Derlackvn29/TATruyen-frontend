e78a8837eebe063bdd56f94c3ef5591242e3de0e
Derlackvn29 <tranhuynhan29012007@gmail.com>
2026-07-01 08:39:17 +0700

    Add main App component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 TATruyen - Story Sharing Platform</h1>
+        <p>Welcome to TATruyen</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
